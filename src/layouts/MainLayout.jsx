a3704301eaa6d862d10e695e1eb11cf6dce35f38
pavanganeshbutha <pavanganeshbutha@gmail.com>
2026-02-18 11:23:21 +0530

    Add folder structure, Routes

---
+++ src/layouts/MainLayout.jsx
@@ -0,0 +1,17 @@
+import Header from "../components/Header";
+import { Outlet } from "react-router-dom";
+import Footer from "../components/Footer";
+
+const MainLayout = () => {
+  return (
+    <div className="flex flex-col border min-h-screen">
+      <Header />
+      <main className="flex-1">
+        <Outlet />
+      </main>
+      <Footer />
+    </div>
+  );
+};
+
+export default MainLayout;
